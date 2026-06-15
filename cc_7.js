5a1a6a8fa6b3c3f4bf9e14c13e1110b8dd9f8bb7
ShakiraAlli <shakiraalli@usf.edu>
2026-06-14 23:46:07 -0400

    Fixed error in index.html and cc_7.js causing cta button to not work

--- cc_7.js
+++ cc_7.js
@@ -1,6 +1,6 @@
 // Use DOM API to select input, button, and cta <h2>
 const headlineInput = document.getElementById('headline-input');
-const updateButton = document.getElementById('update-button');
+const updateButton = document.getElementById('update-headline-btn');
 const ctaHeadline = document.getElementById('cta-headline');
 
 // Add event listener to button to update the CTA headline
